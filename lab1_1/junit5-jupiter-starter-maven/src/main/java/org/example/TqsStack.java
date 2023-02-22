bbed1ab47de15d3d54212c79a10733e234552a21
CtarinaCosta02 <catarinateves02@ua.pt>
2023-02-22 21:37:38 +0000

    begining

---
+++ lab1_1/junit5-jupiter-starter-maven/src/main/java/org/example/TqsStack.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.Stack;
+
+public class TqsStack {
+
+    // create a Stack
+    Stack<Integer> stack = new Stack<>();
+    // do a push(x) operation
+    stack.push(10);
+    // do a pop() operation
+    stack.pop();
+    // do a peek() operation
+    stack.peek();
+    // size: returns the number os items in the stack
+    stack.size();
+    // isEmpty: returns true if the stack is empty
+    stack.isEmpty();
+
+
+}
