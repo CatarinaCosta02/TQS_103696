a5a922be4a4441a0851391dfe718988917f5e0e4
CtarinaCosta02 <catarinateves02@ua.pt>
2023-02-23 00:02:40 +0000

    stack squeleton

--- lab1_1/junit5-jupiter-starter-maven/src/main/java/org/example/TqsStack.java
+++ lab1_1/junit5-jupiter-starter-maven/src/main/java/org/example/TqsStack.java
@@ -4,18 +4,43 @@ import java.util.Stack;
 
 public class TqsStack {
 
-    // create a Stack
-    Stack<Integer> stack = new Stack<>();
-    // do a push(x) operation
-    stack.push(10);
-    // do a pop() operation
-    stack.pop();
-    // do a peek() operation
-    stack.peek();
-    // size: returns the number os items in the stack
-    stack.size();
-    // isEmpty: returns true if the stack is empty
-    stack.isEmpty();
+    private ArrayList<T> stack;
+    private int maxSize;
+
+    public TqsStack(int maxSize) {
+        this.maxSize = maxSize;
+        this.stack = new ArrayList<T>();
+    }
+
+    public void push(T element) {
+        if (this.stack.size() < this.maxSize) {
+            this.stack.add(element);
+        }
+    }
+
+    public T pop() {
+        if (this.stack.size() > 0) {
+            return this.stack.remove(this.stack.size() - 1);
+        }
+        return null;
+    }
+
+    public T peek() {
+        if (this.stack.size() > 0) {
+            return this.stack.get(this.stack.size() - 1);
+        }
+        return null;
+    }
+
+    public int size() {
+        return this.stack.size();
+    }
+
+    public boolean isEmpty() {
+        return this.stack.size() == 0;
+    }
+
+
 
 
 }
