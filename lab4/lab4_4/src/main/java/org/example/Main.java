5d9950361c87dced7df37c404486f40a14600843
CtarinaCosta02 <catarinateves02@ua.pt>
2023-04-03 02:05:01 +0100

    Lab4 almost done

--- lab4/lab4_4/src/main/java/org/example/Main.java
+++ lab4/lab4_4/src/main/java/org/example/Main.java
@@ -1,7 +1,60 @@
 package org.example;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    WebDriver driver;
+
+    @FindBy(name = "fromPort")
+    WebElement fromPort;
+
+    @FindBy(name = "toPort")
+    WebElement toPort;
+
+    @FindBy(css = ".btn-primary")
+    WebElement findFlightsButton;
+
+    @FindBy(css = "tr:nth-child(2) .btn")
+    WebElement selectFlight;
+
+    @FindBy(css = ".btn-primary")
+    WebElement purchaseFlight;
+
+    @FindBy(tagName = "h1")
+    WebElement successMessage;
+
+
+    //Constructor
+    public Main(WebDriver driver){
+        this.driver=driver;
+
+        //Initialise Elements
+        PageFactory.initElements(driver, this);
+        driver.get("http://blazedemo.com/");
     }
+
+    public void selectFlight(String from, String to){
+
+        Select fromPortSelect = new Select(fromPort);
+        fromPortSelect.selectByValue(from);
+
+        Select toPortSelect = new Select(toPort);
+        toPortSelect.selectByValue(to);
+
+        findFlightsButton.click();
+        selectFlight.click();
+        purchaseFlight.click();
+    }
+
+    public String getSuccessMessage(){
+        return successMessage.getText().contains("Thank you for your purchase today!") ? "Success" : "Failure";
+    }
+
+
 }
--- lab4/lab4_4/src/test/java/DockerBuyTripTest.java
+++ lab4/lab4_4/src/test/java/DockerBuyTripTest.java
@@ -1,7 +1,7 @@
-
 import static io.github.bonigarcia.seljup.BrowserType.CHROME;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.example.Main;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.WebDriver;
@@ -10,12 +10,15 @@ import io.github.bonigarcia.seljup.DockerBrowser;
 import io.github.bonigarcia.seljup.SeleniumJupiter;
 
 @ExtendWith(SeleniumJupiter.class)
-class DockerChromeTest {
+public class DockerBuyTripTest {
 
     @Test
     void testChrome(@DockerBrowser(type = CHROME) WebDriver driver) {
+        Main main = new Main(driver);
+        main.selectFlight("Portland", "London");
         driver.get("https://bonigarcia.dev/selenium-webdriver-java/");
         assertThat(driver.getTitle()).contains("Selenium WebDriver");
     }
+
 
 }
