32f594b81d394c4bb83dec6b84c5c0fc3e43ac5a
Catarina Costa <101096418+CatarinaCosta02@users.noreply.github.com>
2023-04-04 16:32:21 +0100

    Lab3 done

---
+++ lab3/Lab3_2/demo/src/test/java/com/example/demo/APITest.java
@@ -0,0 +1,65 @@
+package com.example.demo;
+
+import com.example.Car;
+import com.example.repository.CarRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureTestDatabase
+public class APITest {
+    @LocalServerPort
+    int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private CarRepository carRepository;
+
+    @AfterEach
+    public void resetDb() {
+        carRepository.deleteAll();
+    }
+
+    @Test
+    public void whenValidInput_thenCreateCar() {
+        Car car = new Car("GLA SUV", "Mercedes-Benz");
+        restTemplate.postForEntity("/api/v1/cars/cars", car, Car.class);
+        assertThat(carRepository.findAll()).extracting(Car::getModel).containsOnly("GLA SUV");
+    }
+
+    @Test
+    public void givenCars_whenGetCars_thenStatus200() {
+        createTestCar("GLA SUV", "Mercedes-Benz");
+        createTestCar("C-Class", "Mercedes-Benz");
+
+        ResponseEntity<List<Car>> response = restTemplate.exchange("/api/v1/cars/cars", HttpMethod.GET, null,
+                new ParameterizedTypeReference<List<Car>>() {
+                });
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).extracting(Car::getModel).containsExactly("GLA SUV", "C-Class");
+    }
+
+    private void createTestCar(String model, String maker) {
+        Car car = new Car(model, maker);
+        carRepository.saveAndFlush(car);
+
+    }
+
+
+}
